1ed036ebf8f72e60bdeb0bdd55863d74035acf5b
Leonardo <leonardolombardi@poli.ufrj.br>
2023-03-03 15:46:07 +0000

    Redirect on all routes

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,11 +1,16 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LoaderArgs, MetaFunction } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
 import RouteErrorBoundary from "~/components/views/RouteErrorBoundary";
+import * as Cookies from "~/services/cookies";
 
 export const meta: MetaFunction = () => ({
   title: "Recipe Manager | Home",
   description: "Browse and save appetizing recipes",
 });
+
+export async function loader({ request }: LoaderArgs) {
+  return await Cookies.redirectIfUnauthorized(request);
+}
 
 export default function HomeRoute() {
   return (
